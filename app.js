83be9a1d748926551df6aa361cda71c4da2604f1
Tania Diane Dodson <taniad0916@MacBook.local>
2021-02-09 19:33:33 -0500

    adding scoring

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const startBtn = document.querySelector('#start-button')
 const width = 10
 let nextRandom = 0
 let timerId
+let score = 0
 
 const lTetromino = [
   [1, width+1, width*2+1, 2],
@@ -101,6 +102,7 @@ function freeze() {
     currentPosition = 4
     draw()
     displayShape()
+    addScore()
   }
 }
 
@@ -178,3 +180,20 @@ startBtn.addEventListener('click', () => {
     displayShape()
   }
 })
+
+//add score
+function addScore() {
+  for(let i=0; i<199; i+=width){
+    const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+
+    if(row.every(index => squares[index].classList.contains('taken'))) {
+      score +=10
+      scoreDisplay.innerHTML = score
+      row.forEach(index => {
+        squares[index].classList.remove('taken')
+      })
+      const squaresRemoved = squares.splice(i, width)
+      console.log(squaresRemoved)
+    }
+  }
+}
